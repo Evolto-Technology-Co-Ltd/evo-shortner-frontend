a632a8b16554a45b902c6a2df95fa5098a644348
Arie Brainware <publicu001@protonmail.com>
2022-10-13 16:55:44 +0700

    Adjust title

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const buttonShortener = document.querySelector("#btn-shortener");
 buttonShortener.addEventListener("click", function () {
   const url = document.querySelector("#url");
-  fetch("https://rama-shortner-backend.herokuapp.com/short", {
+  fetch("https://api.evo.st/short", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
