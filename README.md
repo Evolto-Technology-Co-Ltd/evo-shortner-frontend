3f469e6e0234970b771b05536889b2f53be368c7
Arie Brainware <publicu001@protonmail.com>
2022-10-13 16:51:04 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# rama-shortner
+
+Tool to shorten URL (Uniform Resource Locator)
+
+Demo :
+https://youtu.be/h9buJelE8BU
---
+++ js/script.js
@@ -0,0 +1,30 @@
+const buttonShortener = document.querySelector("#btn-shortener");
+buttonShortener.addEventListener("click", function () {
+  const url = document.querySelector("#url");
+  fetch("https://rama-shortner-backend.herokuapp.com/short", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      url: url.value,
+    }),
+    redirect: "follow",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector("#result").value = data.data["key"];
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+function btnCopy() {
+  const copyText = document.querySelector("#result");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  navigator.clipboard.writeText(copyText.value);
+}
